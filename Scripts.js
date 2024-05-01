5306a20bde8bee21b68ccc62f94d9af19149db44
Slava <ivancukvaceslav5@gmail.com>
2024-05-01 21:26:52 +0300

    added delete

--- Scripts.js
+++ Scripts.js
@@ -1,19 +1,28 @@
+var selectedRow=null;
 
 function onFormSubmit(){
-    console.log("the submit function runs!!!");
     var formData = readFormData();
-    //insertNewUser(formData);
+    if(selectedRow == null){
+        insertNewUser(formData);
+        AddUserToDatabase(formData);
+    }
+    else{
+        UpdateRecord(formData);
+    }
+    resetForm();
+}
+
+function AddUserToDatabase(data){
     fetch('http://localhost:3000/api/AddUser', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(data)
     })
     .then(response => {
         if (response.ok) {
             console.log('User added successfully!');
-            resetForm();
         } else {
             console.error('Failed to add user:', response.statusText);
         }
@@ -21,7 +30,6 @@ function onFormSubmit(){
     .catch(error => {
         console.error('Error adding user:', error);
     });
-    resetForm();
 }
 
 function readFormData(){
@@ -48,11 +56,11 @@ function readFormData(){
     cell4 = newRow.insertCell(3);
     cell4.innerHTML = data.telephone;
     cell5 = newRow.insertCell(4);
-    cell5.innerHTML = data.card_number;
+    cell5.innerHTML = data.bankCardNumber;
     cell6 = newRow.insertCell(5);
-    cell6.innerHTML = data.pin_code;
+    cell6.innerHTML = data.pinCode;
     cell7 = newRow.insertCell(6);
-    cell7.innerHTML = data.cvv_code;
+    cell7.innerHTML = data.cvvCode;
     cell8 = newRow.insertCell(7);
     cell8.innerHTML = data.password;
     cell9 = newRow.insertCell(8);
@@ -68,13 +76,33 @@ function resetForm(){
     document.getElementById("pin_code").value = "";
     document.getElementById("cvv_code").value = "";
     document.getElementById("password").value = "";
-    console.log("the reset function runs!!!");
+    selectedRow = null;
 }
 
-function onEdit(){
-
+function onEdit(td){
+    selectedRow = td.parentElement.parentElement;
+    document.getElementById("name").value = selectedRow.cells[0].innerHTML;
+    document.getElementById("age").value = selectedRow.cells[1].innerHTML;
+    document.getElementById("email").value = selectedRow.cells[2].innerHTML;
+    document.getElementById("telephone").value = selectedRow.cells[3].innerHTML;
+    document.getElementById("card_number").value = selectedRow.cells[4].innerHTML;
+    document.getElementById("pin_code").value = selectedRow.cells[5].innerHTML;
+    document.getElementById("cvv_code").value = selectedRow.cells[6].innerHTML;
+    document.getElementById("password").value = selectedRow.cells[7].innerHTML;
 }
 
-function OnDelete(){
+function UpdateRecord(data){
+    selectedRow.cells[0].innerHTML = data.name;
+    selectedRow.cells[1].innerHTML = data.age;
+    selectedRow.cells[2].innerHTML = data.email;
+    selectedRow.cells[3].innerHTML = data.telephone;
+    selectedRow.cells[4].innerHTML = data.bankCardNumber;
+    selectedRow.cells[5].innerHTML = data.pinCode;
+    selectedRow.cells[6].innerHTML = data.cvvCode;
+    selectedRow.cells[7].innerHTML = data.password;
+}
 
+function OnDelete(td){
+    row = td.parentElement.parentElement;
+    document.getElementById("UserTable").deleteRow(row.rowIndex);
 }
